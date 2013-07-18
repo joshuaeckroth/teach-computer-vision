f40b2249d4b59dfc907d1a2d719c0aebbd394cca
Joshua Eckroth <eckroth@cse.ohio-state.edu>
2013-07-18 11:04:10 -0400

    Output videos of background subtraction (original video + bg mask).

--- track/bgsub.cpp
+++ track/bgsub.cpp
@@ -15,39 +15,57 @@ int main()
     } else {
         capture = new VideoCapture("Balcony5_Vis.mpg"); // use input file
     }
-    Mat frame;
-    Mat frame_gray;
-    Mat frame_binary;
-    Mat frame_old;
-    Mat frame_old_binary;
-    Mat frame_sub;
-
     if(capture->isOpened()) {
-        while(true) {
-            frame_old = frame_gray.clone();
-            *capture >> frame;
-            cvtColor(frame, frame_gray, CV_RGB2GRAY);
-            
-            if(!frame_old.empty())
-            {
-                frame_binary = frame_gray.clone();
-                threshold(frame_gray, frame_binary, 128.0, 255.0, THRESH_BINARY);
-                frame_old_binary = frame_old.clone();
-                threshold(frame_old, frame_old_binary, 128.0, 255.0, THRESH_BINARY);
-                frame_sub = frame_binary - frame_old_binary;
-            }
+        Mat frame;
+        Mat frame_gray;
+        Mat frame_binary;
+        Mat frame_old;
+        Mat frame_old_binary;
+        Mat frame_sub;
+        Mat combined;
+        int w, h;
+        *capture >> frame;
+        w = frame.size().width;
+        h = frame.size().height;
 
-            if(!frame.empty())
-            {
-                imshow("Video", frame);
-                if(!frame_sub.empty())
-                {
-                    imshow("Sub", frame_sub);
-                }
-            }
+        VideoWriter writer("bgsub-output.mpg",
+                           CV_FOURCC('P','I','M','1'),
+                           25, cvSize(2*w, h), 1); // 0 means gray, 1 means color
+        if(writer.isOpened()) {
+            while(true) {
+                frame_old = frame_gray.clone();
+                *capture >> frame;
+                cvtColor(frame, frame_gray, CV_RGB2GRAY);
             
-            int key = waitKey(10);
-            if((char)key == 'q') { break; }
+                if(!frame_old.empty())
+                {
+                    frame_binary = frame_gray.clone();
+                    threshold(frame_gray, frame_binary, 128.0, 255.0, THRESH_BINARY);
+                    frame_old_binary = frame_old.clone();
+                    threshold(frame_old, frame_old_binary, 128.0, 255.0, THRESH_BINARY);
+                    frame_sub = frame_binary - frame_old_binary;
+                }
+
+                if(!frame.empty())
+                {
+                    if(!frame_sub.empty())
+                    {
+                        combined = Mat(h, 2*w, CV_8UC3);
+                        Mat left(combined, Rect(0, 0, w, h));
+                        frame.copyTo(left);
+                        Mat right(combined, Rect(w, 0, w, h));
+                        Mat frame_sub_rgb;
+                        cvtColor(frame_sub, frame_sub_rgb, CV_GRAY2RGB);
+                        frame_sub_rgb.copyTo(right);
+                        imshow("Combined", combined);
+
+                        writer << combined;
+                    }
+                }
+            
+                int key = waitKey(10);
+                if((char)key == 'q') { break; }
+            }
         }
     }
 
--- track/bgsubmog.cpp
+++ track/bgsubmog.cpp
@@ -17,30 +17,51 @@ int main()
     } else {
         capture = new VideoCapture("Balcony5_Vis.mpg"); // use input file
     }
-    Mat frame;
-    Mat fgmask;
-    Mat bg;
-
     if(capture->isOpened()) {
-        while(true) {
-            *capture >> frame;
+        Mat frame;
+        Mat fgmask;
+        Mat bg;
+        Mat combined;
+        int w, h;
 
-            if(!frame.empty())
-            {
-                fgmask = frame.clone();
-                backsub(frame, fgmask, 0.1);
-                backsub.getBackgroundImage(bg);
+        *capture >> frame;
+        w = frame.size().width;
+        h = frame.size().height;
 
-                imshow("Video", frame);
-                if(!fgmask.empty())
+        VideoWriter writer("bgsubmog-output.mpg",
+                           CV_FOURCC('P','I','M','1'),
+                           25, cvSize(3*w, h), 1); // 0 means gray, 1 means color
+
+        if(writer.isOpened()) {
+            while(true) {
+                *capture >> frame;
+
+                if(!frame.empty())
                 {
-                    imshow("Foreground mask", fgmask);
-                    imshow("Background", bg);
+                    fgmask = frame.clone();
+                    backsub(frame, fgmask, 0.1);
+                    backsub.getBackgroundImage(bg);
+
+                    if(!fgmask.empty())
+                    {
+                        combined = Mat(h, 3*w, CV_8UC3);
+                        Mat left(combined, Rect(0, 0, w, h));
+                        frame.copyTo(left);
+                        Mat right(combined, Rect(w, 0, w, h));
+                        Mat fgmask_rgb;
+                        cvtColor(fgmask, fgmask_rgb, CV_GRAY2RGB);
+                        fgmask_rgb.copyTo(right);
+                        Mat right2(combined, Rect(2*w, 0, w, h));
+                        bg.copyTo(right2);
+                        imshow("Original / Background subtracted / Background model", combined);
+
+                        writer << combined;
+                    }
                 }
-            }
             
-            int key = waitKey(10);
-            if((char)key == 'q') { break; }
+                int key = waitKey(10);
+                if((char)key == 'q') { break; }
+            }
         }
     }
 
